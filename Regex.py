bb70f3d82820afa698e33b943bdff5f67ce160f0
Alexander R. Taylor <ataylor@IT-007050.local>
2024-11-18 14:08:28 -0500

    Added processing
    NHSecure Browser now processes and created intunewin

---
+++ Regex.py
@@ -0,0 +1,34 @@
+from autopkglib import Processor, ProcessorError
+import re
+
+__all__ = ["Regex"]
+
+
+class Regex(Processor):
+    """This processor filters an incoming string based on the provided regular expression."""
+
+    description = __doc__
+    input_variables = {
+        "string": {
+            "required": True,
+            "description": "String to search.",
+        }
+        "regex": {
+            "required": True,
+            "description": "String to search.",
+        }
+    }
+    output_variables = {
+        "regoutput": {
+        }
+    }
+
+    def main(self):
+        string = self.env.get('string')
+        regex = self.env.get('regex')
+
+        regoutput = re.search(regex, string)
+
+if __name__ == "__main__":
+    PROCESSOR = SampleSharedProcessor()
+    PROCESSOR.execute_shell()
--- vexTournamentManager/vexTournamentManager-Win64.build/read/readme-vexTournamentManager_64_VVeerrssiioonn_ML.txt
+++
@@ -1,6 +0,0 @@
-	SafeExamBrowser x64 for Windows 1.0.0.0 EN
-	Add Desktop Shortcut.
-
-	Release 1.0.0.0, Build 1
-
-20190923, by ETHZ ID-CD AutoPkg
